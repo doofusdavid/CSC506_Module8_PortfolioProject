3647725398beb0e66d9c6540aef6388d19fb456f
David Edwards <david.edwards@gmail.com>
2021-04-25 11:03:59 -0600

    basic profiling being output

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import numpy as np
 import timeit
 import mergesort
 import quicksort
-import radixsort
 import revised_quicksort
 import shellsort
 import insertionsort
 import math
+import cProfile
+import pstats
+import io
+import sys
 
 LIST_SIZE = 500
-MAX_VALUE = 999
+MAX_VALUE = sys.maxsize
 NUM_ITERATIONS = 20
 
 
@@ -23,11 +26,6 @@ def quicksort_test():
     quicksort.quicksort(numbers, 0, len(numbers)-1)
 
 
-def radixsort_test():
-    numbers = list(np.random.randint(low=1, high=MAX_VALUE, size=LIST_SIZE))
-    radixsort.radix_sort(numbers)
-
-
 def revised_quicksort_test():
     numbers = list(np.random.randint(low=1, high=MAX_VALUE, size=LIST_SIZE))
     revised_quicksort.revised_quicksort(numbers, 0, len(numbers)-1, 100)
@@ -36,11 +34,6 @@ def revised_quicksort_test():
 def shellsort_test():
     numbers = list(np.random.randint(low=1, high=MAX_VALUE, size=LIST_SIZE))
     shellsort.shell_sort(numbers, get_shellsort_gaps())
-
-
-def timsort_test():
-    numbers = list(np.random.randint(low=1, high=MAX_VALUE, size=LIST_SIZE))
-    numbers.sort()
 
 
 def insertionsort_test():
@@ -56,34 +49,28 @@ def get_shellsort_gaps():
     return gaps
 
 
-def perform_test():
-    results = {}
-    results["Insertion Sort"] = timeit.timeit(
-        lambda: insertionsort_test(), number=NUM_ITERATIONS)
-    results["Merge Sort"] = timeit.timeit(
-        lambda: mergesort_test(), number=NUM_ITERATIONS)
-    results["Quick Sort"] = timeit.timeit(
-        lambda: quicksort_test(), number=NUM_ITERATIONS)
-    results["Radix Sort"] = timeit.timeit(
-        lambda: radixsort_test(), number=NUM_ITERATIONS)
-    results["Revised Quicksort"] = timeit.timeit(
-        lambda: revised_quicksort_test(), number=NUM_ITERATIONS)
-    results["Shell Sort"] = timeit.timeit(
-        lambda: shellsort_test(), number=NUM_ITERATIONS)
-    results["Tim Sort"] = timeit.timeit(
-        lambda: timsort_test(), number=NUM_ITERATIONS)
-
-    sorted_results = sorted(results.items(), key=lambda x: x[1])
-
-    print("{:<21} | {:>9}".format("List size:", LIST_SIZE))
-    print("{:<21} | {:>9}".format("Maximum Int Value:", MAX_VALUE))
-    print("{:<21} | {:>9}".format("Sorts per algorithm:", NUM_ITERATIONS))
-
-    print("-"*33)
-    print("|{:<20} |{:<10}".format("Sorting Method", "Time"))
-    print("-"*33)
-    for item in sorted_results:
-        print("|{:<20} |{:<10.8f}".format(item[0], item[1]))
+def performtests():
+    insertionsort_test()
+    mergesort_test()
+    quicksort_test()
+    revised_quicksort_test()
+    shellsort_test()
 
 
-perform_test()
+pr = cProfile.Profile()
+pr.enable()
+performtests()
+pr.disable()
+
+s = io.StringIO()
+ps = pstats.Stats(pr, stream=s).sort_stats("cumulative")
+ps.strip_dirs().print_stats()
+
+results = s.getvalue()
+for line in results.splitlines():
+    if "_test" in line:
+        print(line)
+
+
+# value = cProfile.run('shellsort_test()')
+# print(value)
